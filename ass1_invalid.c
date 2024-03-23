6dd87c31986d7071f07c9fd428066b374401f2f0
Nihal T M <nihaltmdev@gmail.com>
2024-03-23 23:00:23 +0530

    Separated valid and invalid test case C files

--- ass1_invalid.c
+++ ass1_invalid.c
@@ -3,14 +3,14 @@
 int main() {
     int a;
     int x,y;
-    int i = 0, j = 1;
+    int i = 0, double j = 1;
     int p = 5, q = 3;
     int i = 10;
-    for(i = 0, j = 0;(i < p) && (j < q);i++, j++) {
+    for(int i = 0,float j = 0;(i < p) && (j < q);i++, j++) {
         a = 10;
     }
 
-    while (p > q)
+    whil (p > q)
     {
         int d = i + 2;
     }
@@ -18,13 +18,14 @@ int main() {
 
     int a = 5, b, c, d = 10;
 
-    int a[10];
+    int a[*]{;
     int a[5][6][7][8];
     int a[4][4], b[5];
 
     switch (a)
     {
     case 10: int b = 10;
+        break;
         break;
     
     case 9: int s = 5;
---
+++ ass1_valid.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+int a;
+int x,y;
+int i = 0, j = 1;
+int p = 5, q = 3;
+int i = 10;
+
+for(i = 0, j = 0;(i < p) && (j < q);i++, j++) {
+a = 10;
+}
+while (p > q)
+{
+int d = i + 2;
+}
+int a = 5, b, c, d = 10;
+
+int a[10];
+int a[5][6][7][8];
+int a[4][4], b[5];
+
+switch (a)
+{
+case 10: int b = 10;
+break;
+case 9: int s = 5;
+default:
+break;
+}
+}
